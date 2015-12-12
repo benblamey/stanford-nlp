cb3d5b48ca433dc3a3ef4b394857c5c405d9cbe3
Ben Blamey <blamey.ben@gmail.com>
2015-12-13 00:31:54 +0100

    Break dependency on benblamey.core

--- src/edu/stanford/nlp/time/distributed/TimeRangeDensity.java
+++ src/edu/stanford/nlp/time/distributed/TimeRangeDensity.java
@@ -1,10 +1,11 @@
 package edu.stanford.nlp.time.distributed;
 
-import benblamey.gnuplot.GnuPlot;
 import edu.stanford.nlp.time.Range;
 import edu.stanford.nlp.time.Time;
 import edu.stanford.nlp.time.distributed.TimeDensityFunction;
 import org.joda.time.DateTime;
+import org.joda.time.Instant;
+import org.joda.time.Seconds;
 
 public class TimeRangeDensity extends TimeDensityFunction {
     private final Range _range;
@@ -38,10 +39,24 @@ public class TimeRangeDensity extends TimeDensityFunction {
         }
         
         return "(" 
-                + "(" + millTimeSecondsExpr + " > " + GnuPlot.toMillTime(_range.begin.getJodaTimeInstant())  + ")"
+                + "(" + millTimeSecondsExpr + " > " + toSecondsSinceY2K(_range.begin.getJodaTimeInstant())  + ")"
                 + " && " 
-                + "(" + millTimeSecondsExpr + " > " + GnuPlot.toMillTime(_range.end.getJodaTimeInstant()) + ")"
+                + "(" + millTimeSecondsExpr + " > " + toSecondsSinceY2K(_range.end.getJodaTimeInstant()) + ")"
                 +")";
     }
+    
+        /**
+     * Seconds since January 1st 2000 is used for gnuplot.
+     * @return 
+     */
+    public static int toSecondsSinceY2K(Instant dateTime) {
+        try {
+            return Seconds.secondsBetween(s_startMillenium, dateTime).getSeconds();
+        } catch (ArithmeticException e) {
+            System.out.println("offending date: " + dateTime.toString());
+            return 0;
+        }
+    }
+    private static final DateTime s_startMillenium = new DateTime(2000, 1, 1, 0, 0);
 
 }
